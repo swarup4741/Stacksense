e11dc71bd18d6a74f342fb96850477ffcba47a97
Swarup Kumar Das <313swarup@gmail.com>
2021-07-29 12:38:10 +0530

    [Improvement] Added alt tags to the image components (#1)

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-import { Input, InputGroup, InputRightElement } from '@chakra-ui/input';
-import { Button } from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { Button } from "@chakra-ui/react";
 
-import { Header } from '../components/Header';
-import Meta from '../partials/meta';
-import { useAuth } from '../lib/auth';
+import { Header } from "../components/Header";
+import Meta from "../partials/meta";
+import { useAuth } from "../lib/auth";
 
 const signup = () => {
   const auth = useAuth();
   const router = useRouter();
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
 
   const [show, setShow] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -36,7 +36,7 @@ const signup = () => {
       <div className="min-h-screen w-screen absolute top-0 bg-black flex justify-center items-center px-6">
         <div className="max-w-sm px-6 py-8 mt-8">
           <form
-            onSubmit={async (e) => {
+            onSubmit={async e => {
               e.preventDefault();
               setLoading(true);
               const USER = await auth.signup(email, pass, name);
@@ -48,7 +48,11 @@ const signup = () => {
               Create your account in
               <Link href="/">
                 <span className=" flex text-indigo-light mt-1 items-center cursor-pointer">
-                  <img src="/assets/logo.svg" className="h-8 mr-1 ml-2" />
+                  <img
+                    src="/assets/logo.svg"
+                    className="h-8 mr-1 ml-2"
+                    alt="stacksense logo"
+                  />
                   Stacksense
                 </span>
               </Link>
@@ -62,7 +66,7 @@ const signup = () => {
               isRequired
               color="#AA8BFF"
               autoComplete="off"
-              onChange={(e) => setName(e.target.value)}
+              onChange={e => setName(e.target.value)}
             />
 
             <Input
@@ -73,18 +77,18 @@ const signup = () => {
               value={email}
               color="#AA8BFF"
               autoComplete="off"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={e => setEmail(e.target.value)}
             />
             <InputGroup>
               <Input
-                type={show ? 'text' : 'password'}
+                type={show ? "text" : "password"}
                 placeholder="Enter your password..."
                 focusBorderColor="purple.400"
                 isRequired
                 value={pass}
                 color="#AA8BFF"
                 autoComplete="off"
-                onChange={(e) => setPass(e.target.value)}
+                onChange={e => setPass(e.target.value)}
               />
               <InputRightElement cursor="pointer">
                 {show ? (
@@ -106,9 +110,9 @@ const signup = () => {
               bg="rgba(116, 66, 255)"
               w="full"
               fontWeight="semibold"
-              _focus={{ outline: 'none' }}
-              _hover={{ bg: 'rgba(85, 48, 190)' }}
-              _active={{ bg: 'rgba(85, 48, 190)' }}
+              _focus={{ outline: "none" }}
+              _hover={{ bg: "rgba(85, 48, 190)" }}
+              _active={{ bg: "rgba(85, 48, 190)" }}
               isLoading={loading}
               loadingText="Signing up..."
               type="submit"
@@ -116,7 +120,7 @@ const signup = () => {
               Sign up
             </Button>
             <p className="text-center">
-              Already have an account?{' '}
+              Already have an account?{" "}
               <Link href="/signin">
                 <a className="text-indigo-light pl-1 hover:underline">
                   Sign in
